280a4f5cc7130f2b58244558b8e24854bf7cfc5e
Henry Rapley <froggywillneverdie@msn.com>
2011-07-25 12:29:06 +0100

    pushing build 11

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from distutils.core import setup
 
 setup(name='Reflex-events',
-    version='1.10',
+    version='1.11',
     description='Event system for Python',
     author='photofroggy',
     author_email='froggywillneverdie@msn.com',
